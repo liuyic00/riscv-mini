83cd823cfc42fddd17e6cb40891327a429e9437b
SeddonShen <283481855@qq.com>
2024-08-16 23:25:13 +0800

    feat: update speccore without tlb mmu

--- src/test/scala/formal/RISCVMiniFormalSpec.scala
+++ src/test/scala/formal/RISCVMiniFormalSpec.scala
@@ -66,7 +66,7 @@ object DefaultCoreConfig {
 }
 
 class RISCVMiniFormalSpec extends AnyFlatSpec with Formal with ChiselScalatestTester {
-  behavior of "RISCVMiniFormal_E1test"
+  behavior of "RISCVMiniFormal"
   // it should "pass simpletest" in {
   //   test(new CoreTester(new Core(DefaultCoreConfig()), "rv32ui-p-simple")).runUntilStop(10)
   // }
@@ -74,10 +74,10 @@ class RISCVMiniFormalSpec extends AnyFlatSpec with Formal with ChiselScalatestTe
 //     (new chisel3.stage.ChiselStage)
 //       .emitSystemVerilog(new Core(DefaultCoreConfig()), Array("--target-dir", "test_run_dir/Elaborate_RVFI"))
 //   }
-//  it should "generate SoCVerilog" in {
-//    (new chisel3.stage.ChiselStage)
-//      .emitSystemVerilog(new CoreSoc(new Core(DefaultCoreConfig())), Array("--target-dir", "test_run_dir/Elaborate_RVFI"))
-//  }
+ it should "generate SoCVerilog" in {
+   (new chisel3.stage.ChiselStage)
+     .emitSystemVerilog(new CoreSoc(new Core(DefaultCoreConfig())), Array("--target-dir", "test_run_dir/Elaborate_RVFI"))
+ }
   it should "pass verify" in {
     verify(new CoreSoc(new Core(DefaultCoreConfig())), Seq(BoundedCheck(20), BtormcEngineAnnotation))
   }
